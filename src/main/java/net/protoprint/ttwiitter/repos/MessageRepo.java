2eec1648643a78ea60ee5dcf806adb99c3cbc4c4
atomink <atomink@gmail.com>
2019-11-25 21:57:30 +0300

    Added Hibernate, filter

---
+++ src/main/java/net/protoprint/ttwiitter/repos/MessageRepo.java
@@ -0,0 +1,11 @@
+package net.protoprint.ttwiitter.repos;
+
+import net.protoprint.ttwiitter.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+}
