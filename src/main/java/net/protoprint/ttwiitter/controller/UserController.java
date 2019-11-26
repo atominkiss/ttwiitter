0eb8c271f386d88819c71b3ae9a9f920d458415f
atomink <atomink@gmail.com>
2019-11-26 16:03:40 +0300

    Added: start adding UserList

---
+++ src/main/java/net/protoprint/ttwiitter/controller/UserController.java
@@ -0,0 +1,19 @@
+package net.protoprint.ttwiitter.controller;
+
+import net.protoprint.ttwiitter.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping
+    public String userList(){
+        return "userList";
+    }
+}
--- src/main/java/net/protoprint/ttwiitter/domain/Role.java
+++ src/main/java/net/protoprint/ttwiitter/domain/Role.java
@@ -3,7 +3,7 @@ package net.protoprint.ttwiitter.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER, ADMIN;
 
     @Override
     public String getAuthority() {
