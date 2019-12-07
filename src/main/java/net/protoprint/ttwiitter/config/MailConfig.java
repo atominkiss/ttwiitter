3f759f7c1363323ae5629d5422006b243d1e7fe7
atomink <atomink@gmail.com>
2019-12-07 15:37:56 +0300

    Adding activation of user. User creating and activated in database, but e-mail's with activation code don't sending.

---
+++ src/main/java/net/protoprint/ttwiitter/config/MailConfig.java
@@ -0,0 +1,48 @@
+package net.protoprint.ttwiitter.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+    @Value("${sping.mail.host}")
+    private String host;
+
+    @Value("${sping.mail.username}")
+    private String username;
+
+    @Value("${sping.mail.password}")
+    private String password;
+
+    @Value("${sping.mail.port}")
+    private int port;
+
+    @Value("${sping.mail.protocol}")
+    private String protocol;
+
+    @Value("${mail.debug}")
+    private String debug;
+    @Bean
+    public JavaMailSender getMailSender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties properties = mailSender.getJavaMailProperties();
+
+        properties.setProperty("mail.transport.protocol", protocol);
+        properties.setProperty("mail.debug", debug);
+
+
+        return mailSender;
+    }
+}
--- src/main/java/net/protoprint/ttwiitter/controller/RegistrationController.java
+++ src/main/java/net/protoprint/ttwiitter/controller/RegistrationController.java
@@ -1,19 +1,20 @@
 package net.protoprint.ttwiitter.controller;
 
-import net.protoprint.ttwiitter.domain.Role;
 import net.protoprint.ttwiitter.domain.User;
-import net.protoprint.ttwiitter.repos.UserRepo;
+import net.protoprint.ttwiitter.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import java.util.Collections;
+
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
     @Autowired
-    private UserRepo userRepo;
+    private UserService userService;
 
     @GetMapping("/registration")
     public String registration(){
@@ -22,18 +23,26 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String addUser(User user, Map<String, Object> model){
-        User userFromDB = userRepo.findByUsername(user.getUsername());
 
-        if (userFromDB != null){
+        if (!userService.addUser(user)){
             model.put("message", "User is already exist!");
             return "registration";
         }
 
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepo.save(user);
         return "redirect:/login";
     }
 
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code){
+        boolean isActivated = userService.activateUser(code);
+
+        if (isActivated){
+            model.addAttribute("message", "User successfully activated!");
+        } else {
+            model.addAttribute("message", "Activation code is not found!");
+        }
+
+        return "login";
+    }
 
 }
