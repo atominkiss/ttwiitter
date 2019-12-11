e4046c6dec226fe7b9003fce7ed7d32fafed54b8
atomink <atomink@gmail.com>
2019-12-11 14:23:37 +0300

    Added DB migration (V1_ creating DB, V2_ adding first user -ADMIN) and password encryption(V3_).

--- src/main/java/net/protoprint/ttwiitter/controller/UserController.java
+++ src/main/java/net/protoprint/ttwiitter/controller/UserController.java
@@ -2,31 +2,30 @@ package net.protoprint.ttwiitter.controller;
 
 import net.protoprint.ttwiitter.domain.Role;
 import net.protoprint.ttwiitter.domain.User;
-import net.protoprint.ttwiitter.repos.UserRepo;
+import net.protoprint.ttwiitter.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     @Autowired
-    private UserRepo userRepo;
+    private UserService userService;
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     public String userList(Model model){
-        model.addAttribute("user",userRepo.findAll());
+        model.addAttribute("user",userService.findAll());
         return "userList";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("{user}")
     public String userEditForm(@PathVariable User user, Model model){
         model.addAttribute("user",user);
@@ -34,28 +33,35 @@ public class UserController {
         return "userEdit";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping
     public String usersave(
             @RequestParam String username,
             @RequestParam Map<String, String> form,
             @RequestParam ("userId") User user
     ){
-        user.setUsername(username);
 
-        Set<String> roles = Arrays.stream(Role.values())
-                .map(Role::name)
-                .collect(Collectors.toSet());
-
-        user.getRoles().clear();
-
-        for (String key : form.keySet()) {
-            if (roles.contains(key)){
-                user.getRoles().add(Role.valueOf(key));
-            }
-        }
-
-        userRepo.save(user);
+        userService.saveUser(user, username, form);
 
         return "redirect:/user";
+    }
+
+    @GetMapping("profile")
+    public String getProfile(Model model, @AuthenticationPrincipal User user) {
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+
+        return "profile";
+    }
+
+    @PostMapping("profile")
+    public String updateProfile(
+            @AuthenticationPrincipal User user,
+            @RequestParam String password,
+            @RequestParam String email
+    ){
+        userService.updateProfile(user, password, email);
+
+        return "redirect:/user/profile";
     }
 }
--- src/main/java/net/protoprint/ttwiitter/domain/Message.java
+++ src/main/java/net/protoprint/ttwiitter/domain/Message.java
@@ -11,7 +11,7 @@ import java.time.LocalTime;
 public class Message {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    private Integer id;
+    private Long id;
 
     private String text;
     private String tag;
@@ -56,11 +56,11 @@ public class Message {
         return text;
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
