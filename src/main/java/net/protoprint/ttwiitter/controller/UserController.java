1a7da99c0310862d903a4d55a5a6af9b2afa650a
atomink <atomink@gmail.com>
2019-12-20 23:01:06 +0300

    Added subscriptions and subscribers

--- src/main/java/net/protoprint/ttwiitter/controller/UserController.java
+++ src/main/java/net/protoprint/ttwiitter/controller/UserController.java
@@ -16,32 +16,33 @@ import java.util.Map;
 @RequestMapping("/user")
 public class UserController {
     @Autowired
-    private UserService userService;
+    private UserService userSevice;
 
     @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
-    public String userList(Model model){
-        model.addAttribute("user",userService.findAll());
+    public String userList(Model model) {
+        model.addAttribute("users", userSevice.findAll());
+
         return "userList";
     }
 
     @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("{user}")
-    public String userEditForm(@PathVariable User user, Model model){
-        model.addAttribute("user",user);
+    public String userEditForm(@PathVariable User user, Model model) {
+        model.addAttribute("user", user);
         model.addAttribute("roles", Role.values());
+
         return "userEdit";
     }
 
     @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping
-    public String usersave(
+    public String userSave(
             @RequestParam String username,
             @RequestParam Map<String, String> form,
-            @RequestParam ("userId") User user
-    ){
-
-        userService.saveUser(user, username, form);
+            @RequestParam("userId") User user
+    ) {
+        userSevice.saveUser(user, username, form);
 
         return "redirect:/user";
     }
@@ -59,9 +60,47 @@ public class UserController {
             @AuthenticationPrincipal User user,
             @RequestParam String password,
             @RequestParam String email
-    ){
-        userService.updateProfile(user, password, email);
+    ) {
+        userSevice.updateProfile(user, password, email);
 
         return "redirect:/user/profile";
+    }
+
+    @GetMapping("subscribe/{user}")
+    public String subscribe(
+            @AuthenticationPrincipal User currentUser,
+            @PathVariable User user
+    ) {
+        userSevice.subscribe(currentUser, user);
+
+        return "redirect:/user-messages/" + user.getId();
+    }
+
+    @GetMapping("unsubscribe/{user}")
+    public String unsubscribe(
+            @AuthenticationPrincipal User currentUser,
+            @PathVariable User user
+    ) {
+        userSevice.unsubscribe(currentUser, user);
+
+        return "redirect:/user-messages/" + user.getId();
+    }
+
+    @GetMapping("{type}/{user}/list")
+    public String userList(
+            Model model,
+            @PathVariable User user,
+            @PathVariable String type
+    ) {
+        model.addAttribute("userChannel", user);
+        model.addAttribute("type", type);
+
+        if ("subscriptions".equals(type)) {
+            model.addAttribute("users", user.getSubscriptions());
+        } else {
+            model.addAttribute("users", user.getSubscribers());
+        }
+
+        return "subscriptions";
     }
 }
